61c167a95fb2d0ee3d6e9a769853b5002bebd270
Darshan Gowda G <darshangowdag.2002@gmail.com>
2023-12-09 10:46:35 +0530

    try.java

---
+++ Day - 1 (Data Types)/try.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = "HackerRank "
+
+new_i = int(input())
+new_d = float(input())
+new_s = input()
+
+sum_i = i + new_i
+sum_d = d + new_d
+sum_s = s + new_s
+
+print(sum_i)
+print("{:.1f}".format(sum_d))
+print(sum_s)
